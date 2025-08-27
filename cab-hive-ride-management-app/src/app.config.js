501aa09e74910001d600672c6636032e8b970920
nicccce <lin2832856512@163.com>
2025-08-27 14:47:18 +0800

    feat：配置了腾讯位置的定位插件

--- cab-hive-ride-management-app/src/app.config.js
+++ cab-hive-ride-management-app/src/app.config.js
@@ -12,6 +12,12 @@ export default defineAppConfig({
     'pages/audit-detail/index',
     'pages/test-vehicle-card/index'
   ],
+  // 配置 permission
+  permission: {
+    'scope.userLocation': {
+      desc: '你的位置信息将用于小程序定位' // 这里会显示在授权弹窗上，请务必填写清晰
+    }
+  },
   tabBar: {
     custom: false,
     color: '#6b7280',
@@ -45,5 +51,11 @@ export default defineAppConfig({
     navigationBarTitleText: '智蜂出行',
     navigationBarTextStyle: 'white',
     backgroundColor: '#f9fafb'
+  },
+  plugins: {
+    chooseLocation: {
+      version: "1.0.12",
+      provider: "wx76a9a06e5b4e693e"
+    }
   }
 })
